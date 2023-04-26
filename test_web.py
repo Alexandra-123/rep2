25e5cde91cf3d4bd4adaa086d819407f8ffa235a
Alexandra-123 <78919274+Alexandra-123@users.noreply.github.com>
2023-04-26 16:01:22 +0300

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -5,5 +5,5 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class Tests:
     def test_title(self):
-        self.driver.get("https://ficbook.net")
-        assert self.driver.title == "Книга Фанфиков - более 25000 фэндомов, ориджиналы от популярных авторов"
+        self.driver.get("https://ru.wikipedia.org/wiki/Заглавная_страница")
+        assert self.driver.title == "Википедия — свободная энциклопедия"
